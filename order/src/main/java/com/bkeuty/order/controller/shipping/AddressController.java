080bf695da20692b4f94df8cdd6409b82b3b8ee7
LangTuDaTinh <quangviet210804@gmail.com>
2026-04-19 11:38:03 +0700

    change: Add API Gateway config for Shipping Service and Payment Service

--- order/src/main/java/com/bkeuty/order/controller/shipping/AddressController.java
+++ order/src/main/java/com/bkeuty/order/controller/shipping/AddressController.java
@@ -1,37 +1,37 @@
-package com.bkeuty.order.controller.shipping;
-
-import com.bkeuty.order.dto.shipping.GHNDistrictListResponse;
-import com.bkeuty.order.dto.shipping.GHNProvinceListResponse;
-import com.bkeuty.order.dto.shipping.GHNWardListResponse;
-import com.bkeuty.order.service.shipping.AddressService;
-import com.bkeuty.order.service.shipping.ShippingService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
-
-@RestController
-@RequestMapping("/api/address")
-public class AddressController {
-    private final AddressService addressService;
-    AddressController(AddressService addressService) {
-        this.addressService = addressService;
-    }
-
-    @GetMapping("/province")
-    public ResponseEntity<Mono<GHNProvinceListResponse>> getGHNProvinceList() {
-        return ResponseEntity.ok(addressService.getGHNProvinceList());
-    }
-    @GetMapping("/district")
-    public ResponseEntity<Mono<GHNDistrictListResponse>> getGHNDistrictList(@RequestParam String provinceId) {
-        return ResponseEntity.ok(addressService.getGHNDistrictList(provinceId));
-    }
-    @GetMapping("/ward")
-    public ResponseEntity<Mono<GHNWardListResponse>> getGHNWardList(@RequestParam String districtId) {
-        return ResponseEntity.ok(addressService.getGHNWardList(districtId));
-    }
-
-
-}
+//package com.bkeuty.order.controller.shipping;
+//
+//import com.bkeuty.order.dto.shipping.GHNDistrictListResponse;
+//import com.bkeuty.order.dto.shipping.GHNProvinceListResponse;
+//import com.bkeuty.order.dto.shipping.GHNWardListResponse;
+//import com.bkeuty.order.service.shipping.AddressService;
+//import com.bkeuty.order.service.shipping.ShippingService;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import reactor.core.publisher.Mono;
+//
+//@RestController
+//@RequestMapping("/api/address")
+//public class AddressController {
+//    private final AddressService addressService;
+//    AddressController(AddressService addressService) {
+//        this.addressService = addressService;
+//    }
+//
+//    @GetMapping("/province")
+//    public ResponseEntity<Mono<GHNProvinceListResponse>> getGHNProvinceList() {
+//        return ResponseEntity.ok(addressService.getGHNProvinceList());
+//    }
+//    @GetMapping("/district")
+//    public ResponseEntity<Mono<GHNDistrictListResponse>> getGHNDistrictList(@RequestParam String provinceId) {
+//        return ResponseEntity.ok(addressService.getGHNDistrictList(provinceId));
+//    }
+//    @GetMapping("/ward")
+//    public ResponseEntity<Mono<GHNWardListResponse>> getGHNWardList(@RequestParam String districtId) {
+//        return ResponseEntity.ok(addressService.getGHNWardList(districtId));
+//    }
+//
+//
+//}
--- order/src/main/java/com/bkeuty/order/controller/shipping/ShippingController.java
+++ order/src/main/java/com/bkeuty/order/controller/shipping/ShippingController.java
@@ -1,29 +1,29 @@
-package com.bkeuty.order.controller.shipping;
-
-import com.bkeuty.order.dto.shipping.*;
-import com.bkeuty.order.service.shipping.ShippingService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
-
-@RestController
-@RequestMapping("/api/shipping")
-public class ShippingController {
-    private final ShippingService shippingService;
-    public ShippingController(ShippingService shippingService) {
-        this.shippingService = shippingService;
-    }
-
-    @PostMapping("/fee")
-    public Mono<CalShippingFeeResponseDto> calShippingFee(@RequestBody CalShippingFeeDto calShippingFeeDto) {
-        return shippingService.calShippingFee(calShippingFeeDto);
-    }
-    @PostMapping("/leadtime")
-    public Mono<CalShippingTimeResponseDto> calShippingTime(@RequestBody CalShippingTimeDto calShippingTimeDto) {
-        return shippingService.calShippingTime(calShippingTimeDto);
-    }
-
-
-}
+//package com.bkeuty.order.controller.shipping;
+//
+//import com.bkeuty.order.dto.shipping.*;
+//import com.bkeuty.order.service.shipping.ShippingService;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import reactor.core.publisher.Mono;
+//
+//@RestController
+//@RequestMapping("/api/shipping")
+//public class ShippingController {
+//    private final ShippingService shippingService;
+//    public ShippingController(ShippingService shippingService) {
+//        this.shippingService = shippingService;
+//    }
+//
+//    @PostMapping("/fee")
+//    public Mono<CalShippingFeeResponseDto> calShippingFee(@RequestBody CalShippingFeeDto calShippingFeeDto) {
+//        return shippingService.calShippingFee(calShippingFeeDto);
+//    }
+//    @PostMapping("/leadtime")
+//    public Mono<CalShippingTimeResponseDto> calShippingTime(@RequestBody CalShippingTimeDto calShippingTimeDto) {
+//        return shippingService.calShippingTime(calShippingTimeDto);
+//    }
+//
+//
+//}
